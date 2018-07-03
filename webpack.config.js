c1fbd4e7cd1649fea70a91dda72a21e54cb62b9b
rajma <rajan.mahadevan@gmail.com>
2018-07-03 19:19:19 -0400

    Additions

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,51 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    {
+                        loader: "style-loader",
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            sourceMap: true,
+                            plugins: () => [
+                                autoprefixer(['last 2 versions']),
+                            ],
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader"
+                }
             }
-        ]
+        ],
     },
     plugins: [htmlPlugin]
 };
